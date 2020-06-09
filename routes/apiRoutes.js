4c88396288bb59c105a3b03bcc2f800401240125
hank amadeus <hankamadeus@email.com>
2020-06-08 21:19:24 -0400

    lost track need to refrence later

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,38 @@
 const router = require("express").Router();
 
-// Route for saving activity
+const activities = [];
 
+// Route for saving activity
+router.post("/activities", (req, res) => {
+    const { mileage, duration, date } = req.body;
+    if (!mileage) {
+        return res.status(400).json({
+            success: false,
+            message: "Mileage is required"
+        })
+    }
+    if (!duration) {
+        return res.status(400).json({
+            success: false,
+            message: "duration is required"
+        })
+    }
+    if (!date) {
+        return res.status(400).json({
+            success: false,
+            message: "Date is required"
+        })
+    }
+    activities.push(req.body);
+    res.json({
+        activities: activities,
+        success: true
+    });
+
+})
 // Route for getting activity data by duration
 
 // route for getting activity data by mileage
-router.post;
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = 8080;
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Routes
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
